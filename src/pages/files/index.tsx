64f2a5e1b62b01985c1de7ef1a7316cff7e0c65a
sultondev <sultondeveloper@gmail.com>
2023-04-15 15:24:55 +0500

    fix: fetch after upload

--- src/pages/files/index.tsx
+++ src/pages/files/index.tsx
@@ -2,18 +2,24 @@ import { ChangeEvent, useState } from "react";
 import { useTranslation } from "react-i18next";
 import { toast } from "react-toastify";
 import { authProtectedApi } from "../../config/axios.config";
-import { ADMIN_FILE_UPLOAD, GET_FILE_STATUS } from "../../config/url_helpers";
+import { ADMIN_FILE_UPLOAD, GET_FILE_STATUS, GET_FILE_TABLES } from "../../config/url_helpers";
 import { Button, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle } from "@mui/material";
+import TheTable from "../../components/UI/TheTable";
+import { FileTyping } from "../../typing/types/Files.type";
 
 const FilesPage = () => {
     const [file, setFile] = useState(null)
     const { t } = useTranslation()
 
     const [open, setOpen] = useState(false);
-    const [fileId, setFileId] = useState('')
+    const [taskId, setTaskId] = useState('')
     const [errorLines, setErrorLines] = useState<number[]>([])
     const [fileUploading, setFileUploading] = useState(false)
     const [fileErrorChecking, setFileChecking] = useState(false)
+    const [tables, setTables] = useState<FileTyping[]>([])
+    const [nextPage, setNextPage] = useState('')
+    const [fileId, setFileId] = useState(0)
+    
     const handleClickOpen = () => {
         setOpen(true);
     };
@@ -48,8 +54,8 @@ const FilesPage = () => {
                 .post(ADMIN_FILE_UPLOAD, fileData)
                 .then((res) => {
                     console.log(res)
-                    setFileId(res.data.task_id)
-                    checkLinesForErrors(res.data.task_id)
+                    setTaskId(res.data.task_id)
+                    checkLinesForErrors(res.data.task_id, res.data.id)
                     toast.success(t('upload_file_success'))
                 })
                 .catch((err) => {
@@ -63,9 +69,9 @@ const FilesPage = () => {
 
     }
 
-    function checkLinesForErrors(taskId: string) {
+    async function checkLinesForErrors(taskId: string, fileId: number) {
         setFileChecking(true)
-        authProtectedApi
+        await authProtectedApi
             .get(GET_FILE_STATUS + `?task_id=${taskId}`)
             .then((res) => {
                 console.log(res)
@@ -73,6 +79,8 @@ const FilesPage = () => {
                     const errors = Object.keys(res.data.result.message)
                     console.log(errors)
                     setErrorLines(...errors)
+                } else {
+                    getTables(fileId)
                 }
             })
             .finally(() => {
@@ -83,6 +91,18 @@ const FilesPage = () => {
 
     function deleteFile(id: string | number) {
 
+    }
+
+    function getTables(fileId: number) {
+        authProtectedApi
+            .get(GET_FILE_TABLES+fileId)
+            .then((res) => {
+                console.log(res)
+                if(res.status === 200) {
+                    setTables(res.data.results.table)
+                    setNextPage(res.data.next)
+                }
+            })
     }
 
     return (
@@ -134,6 +154,58 @@ const FilesPage = () => {
                 }
             </Dialog>
 
+
+            <TheTable classes="responsive-table">
+        <>
+          <thead>
+            <tr className="text-xs text-[#B1B1B8] uppercase text-center">
+              <th className="text-left px-4">#</th>
+              <th className=" text-left">{t('table_head_name')}</th>
+              <th className=" text-left">{t('table_head_special_id')}</th>
+              <th className=" text-left">{t('table_head_username')}</th>
+              <th className=" text-left">{t('table_head_region')}</th>
+              <th className=" text-left">{t('table_head_district')}</th>
+              <th className=" text-center p-1">{t('table_head_actions')}</th>
+            </tr>
+          </thead>
+          <tbody>
+
+
+            {
+              tables && tables.map((item, idx) => {
+                return (
+                  <tr className="" key={item.phone+item.id}>
+                    <td className="py-[23px]  first-td px-4">
+                      {idx + 1}
+                    </td>
+                    <td className="">
+                      {item.specialist ?? t('table_body_empty')}
+                    </td>
+                    <td className="">
+                      {item.id ?? t('table_body_empty')}
+                    </td>
+                    <td className="">
+                      {item.username ?? t('table_body_empty')}
+                    </td>
+                    <td className="">
+                      {item.region ?? t('table_body_empty')}
+                    </td>
+                    <td className="">
+                      {item.district ?? t('table_body_empty')}
+                    </td>
+                    <td className="last-td text-center">
+                      <button>
+                        edit
+                      </button>
+                    </td>
+                  </tr>
+                )
+              })
+            }
+
+          </tbody>
+        </>
+      </TheTable >
         </div>)
 }
 
